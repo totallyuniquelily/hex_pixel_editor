7dcd7d58e35f1e13b669403e59b80b4862087ef2
Lily <34397452+totallyuniquelily@users.noreply.github.com>
2024-01-17 20:26:11 +0100

    half-written prompt

--- src/main.rs
+++ src/main.rs
@@ -28,24 +28,72 @@ async fn main() -> Result<()> {
         inner_width: 208.0,
         visible: true,
     };
+    /// placeholder, whether currently asking for palette indexes to swap
+    let mut prompt = false;
+    let mut promptbuf = String::new();
     loop {
         clear_background(BG_COLOR);
 
-        // Handle input
-        // Scale buttons
-        if is_key_pressed(KeyCode::Equal) {
-            img_scale += 0.1;
+        if !prompt {
+            // Handle input
+            // Scale buttons
+            if is_key_pressed(KeyCode::Equal) {
+                img_scale += 0.1;
+            }
+            if is_key_pressed(KeyCode::Minus) {
+                img_scale -= 0.1;
+            }
+            if is_key_pressed(KeyCode::P) {
+                panel.visible = !panel.visible;
+            }
+
+            if is_key_pressed(KeyCode::T) {
+                prompt = true;
+                // clear char buffer just in case
+                while get_char_pressed().is_some() {}
+            }
         }
-        if is_key_pressed(KeyCode::Minus) {
-            img_scale -= 0.1;
-        }
-        if is_key_pressed(KeyCode::P) {
-            panel.visible = !panel.visible;
+        if prompt && is_key_pressed(KeyCode::Enter) {
+            get_char_pressed();
+            // implement swap here ig
+            promptbuf.clear();
+            prompt = false;
         }
 
         // Draw Image and panel
         panel.draw_image(img.texture(), img_scale);
         panel.draw_panel(&img);
+
+        if prompt {
+            const text: &str = "Palette indexes to swap (separate with space):";
+            if let Some(ch) = get_char_pressed() {
+                if !ch.is_control() {
+                    promptbuf.push(ch);
+                    println!("adding {:?}", ch);
+                }
+            }
+            let dim1 = measure_text(text, None, 20, 1.0);
+            let dim2 = measure_text(&promptbuf, None, 20, 1.0);
+            draw_rectangle(
+                0.0,
+                screen_height() - dim1.height * 1.5,
+                dim1.width + dim2.width,
+                screen_height(),
+                BLACK,
+            );
+            draw_text_ex(
+                text,
+                0.0,
+                screen_height() - dim1.height / 2.,
+                TextParams::default(),
+            );
+            draw_text_ex(
+                &promptbuf,
+                dim1.width,
+                screen_height() - dim2.height / 2.,
+                TextParams::default(),
+            );
+        }
 
         next_frame().await;
     }
